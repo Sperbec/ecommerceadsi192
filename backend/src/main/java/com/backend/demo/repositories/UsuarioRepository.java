e065f7b85d0c9a07534786245244c9eb0652b8df
Sperbec <71613637+Sperbec@users.noreply.github.com>
2021-12-02 20:24:28 -0500

    Trabajaremos con framework Laravel

--- backend/src/main/java/com/backend/demo/repositories/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.demo.repositories;
-
-import com.backend.demo.models.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {}

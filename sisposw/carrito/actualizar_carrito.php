38c59ed74b3ebeb90649149e2e67c3de9d2b4dd7
Sperbec <71613637+Sperbec@users.noreply.github.com>
2021-12-06 21:59:46 -0500

    Directorio eliminado

--- sisposw/carrito/actualizar_carrito.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require "funciones.php";
-$obj= new Funciones();
-
-$m = $obj->actualizarCarrito($_POST['ArtCant'],$_POST['ArtCod'], session_id());
-
-echo $m;
-
- ?>
--- sisposw/reportes/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- $con=mysqli_connect("localhost","root","","sispow") or 
-      die("Problemas con la conexión a la base de datos".  mysqli_error());
-	  
-    
-     ?>
